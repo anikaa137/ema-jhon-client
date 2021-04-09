474a86b875908e9ffe8c19953e15249b9e875cc5
anikaa137 <anikaa137@gmail.com>
2021-04-09 23:22:03 +0600

    config change

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,17 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCYoruiTRCtk-WOpiR-JHxTTF6QwXkIIv4",
+//   authDomain: "fir-app-a9511.firebaseapp.com",
+//   projectId: "fir-app-a9511",
+//   storageBucket: "fir-app-a9511.appspot.com",
+//   messagingSenderId: "93050616423",
+//   appId: "1:93050616423:web:6d1b54217eb7bcfaf932c2"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCYoruiTRCtk-WOpiR-JHxTTF6QwXkIIv4",
-  authDomain: "fir-app-a9511.firebaseapp.com",
-  projectId: "fir-app-a9511",
-  storageBucket: "fir-app-a9511.appspot.com",
-  messagingSenderId: "93050616423",
-  appId: "1:93050616423:web:6d1b54217eb7bcfaf932c2"
+  apiKey: "AIzaSyAQfE4Bg_4VMFpCi_8yzCsKydUMHuqY5N0",
+  authDomain: "ema-jhon-c4f9c.firebaseapp.com",
+  projectId: "ema-jhon-c4f9c",
+  storageBucket: "ema-jhon-c4f9c.appspot.com",
+  messagingSenderId: "676657182692",
+  appId: "1:676657182692:web:54e5502916b7228221f9f1"
 };
   export default firebaseConfig;
